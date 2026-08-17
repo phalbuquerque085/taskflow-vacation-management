faff611e8380bd8239e628c039db38f3572ba715
paulo.albuquerque@timwetech.com <paulo.albuquerque@timwetech.com>
2026-08-17 12:33:20 -0300

    feat(backend): inicializacao do schema do banco de dados, mock de dados iniciais e ambiente docker configurado

---
+++ backend/src/main/java/com/taskflow/vacation/VacationManagementApplication.java
@@ -0,0 +1,12 @@
+package com.taskflow.vacation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacationManagementApplication.class, args);
+    }
+}
